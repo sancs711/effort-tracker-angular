ac34651791b0e22c9412a90eb5f3a7d846bf511b
Madhu Ganga <madhu.ganga@accenture.com>
2018-04-13 14:52:37 -0700

    Home Controller

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,18 +13,7 @@ export class HomeComponent implements OnInit {
     console.log('Home Init');
   }
    
-/*getDateRangeOfWeek(weekNo){
-    var d1 = new Date();
-    var numOfdaysPastSinceLastMonday = eval(d1.getDay()- 1);
-    d1.setDate(d1.getDate() - numOfdaysPastSinceLastMonday);
-    var weekNoToday = d1.getWeek();
-    var weeksInTheFuture = eval( weekNo - weekNoToday );
-    d1.setDate(d1.getDate() + eval( 7 * weeksInTheFuture ));
-    var rangeIsFrom = eval(d1.getMonth()+1) +"/" + d1.getDate() + "/" + d1.getFullYear();
-    d1.setDate(d1.getDate() + 6);
-    var rangeIsTo = eval(d1.getMonth()+1) +"/" + d1.getDate() + "/" + d1.getFullYear() ;
-    return rangeIsFrom + " to "+rangeIsTo;
-}*/
+
 }
 
  
